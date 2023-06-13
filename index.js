cb19c24cd716b05017b1c8e9c5ea7cbb61b6f31d
Martin Muriuki <helheim254@gmail.com>
2023-06-13 20:58:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1;
+//*10 -> base 10, *100 -> base 100...
+let num3 = 12;
+let num4 = 8;
+let mod = num3 % num4;
+
+let max = Math.max(14, 19, 5, 12, 20);
